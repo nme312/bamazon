fbd559a891a3c07e793a2a78a3ec3554c9002dab
SkyNet <nme312.noreply.github.com>
2018-05-19 12:55:58 -0500

    complete bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,37 +12,61 @@ var connection = mysql.createConnection({
 
     // Your password
     password: "root",
-    database: "bamazonDB"
+    database: "bamazondb"
 });
 
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    runMenu();
+    displayAll();
 });
 
-function runMenu() {
-    inquirer.prompt([
-        {
-            type: "list",
-            name: "actions",
-            message: "Please Choose An Option",
-            choices: ["Display All Products"]
+function displayAll() {
+    connection.query("SELECT * FROM products;", function (err, data) {
+        if (err) {
+            throw err;
         }
-    ]).then(function (choice) {
-        switch (choice) {
-            case "Display All Products":
-                displayAll();
-                break;
+        for (var i = 0; i < data.length; i++) {
+            console.log(`${data[i].item_id} | ${data[i].product_name} | $${data[i].price}`);
         }
+        start();
     });
 }
 
-function displayAll() {
-    connection.query("SELECT * FROM products", function (err, data) {
-        for (var i = 0; i < data.length; i++) {
-            console.log(data);
+function start() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "item_id",
+            message: "Please Enter The ID Of The Product You Would Like To Purchase",
+        },
+        {
+            type: "input",
+            name: "order",
+            message: "How Many Of This Item Would You Like To Purchase ?"
         }
-        runMenu();
+    ]).then(function (response) {
+        connection.query("select * from products where item_id = ?", response.item_id, function (err, data) {
+            if (err) {
+                throw err;
+            }
+            if (response.order <= data[0].stock_quantity) {
+                data[0].stock_quantity -= response.order;
+                connection.query("update products set stock_quantity = ? where item_id = ?", [data[0].stock_quantity, response.item_id], function (err, data) {
+                    // console.log(`${data[i].item_id} | ${data[i].product_name} | $${data[i].price}`)
+                    if (err) {
+                        throw err;
+                    }
+                    connection.query("select * from products where item_id = ?", response.item_id, function (err, data) {
+                        console.log(`Purchase Successful, Your Total Is $${data[0].price * response.order}`);
+                        displayAll();
+                    });
+                });
+            } else {
+                console.log("Insufficient Quantity");
+                displayAll();
+            }
+        })
     });
 }
+
